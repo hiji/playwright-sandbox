bc237ae8afa4dcdfe9c50a253343076f9ae00419
Shinya Hijiri <shinya.hijiri@gmail.com>
2020-06-06 20:01:15 +0900

    Playwrightでブラウザ操作

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,7 +1,35 @@
+import { chromium, Page, ChromiumBrowser, CDPSession } from "playwright";
+
+let browser: ChromiumBrowser;
+let page: Page;
+
 describe("example", () => {
 
-  test("sucdess", () => {
-    expect(1).toBe(1);
+  beforeAll(async () => {
+    browser = await chromium.launch({headless: false, slowMo: 10});
+    page = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    await page.close();
+    await browser.close();
+  });
+
+  test('Google search', async () => {
+    await page.goto("http://www.google.co.jp");
+
+    await page.type('css=input', 'wiki');
+    await Promise.all([
+      page.waitForNavigation(),
+      page.keyboard.press('Enter')
+    ]);
+
+    await page.click('css=div[aria-label="消去"]');
+    await page.type('css=input[title="検索"]', 'youtube');
+    await Promise.all([
+      page.waitForNavigation(),
+      page.keyboard.press('Enter')
+    ]);
   });
 
 });
